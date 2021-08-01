95c36d524ad663fd152e4a0309a11d053675474c
s3antuan <87588913+s3antuan@users.noreply.github.com>
2021-08-01 16:05:44 +0800

    update

---
+++ commands/text/seiso.js
@@ -0,0 +1,10 @@
+const random = require('random');
+module.exports = {
+    name: 'せいそ',
+    aliases: ['seiso', '清楚'],
+    args: false,
+    execute(message, args) {
+        const msg = ['おしっこ', 'おしっこ💦'];
+      return message.channel.send(msg[random.int((min = 0), (max = msg.length - 1))]);
+    },
+  };
